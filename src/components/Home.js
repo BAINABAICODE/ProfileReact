d45419657056e6d50173f69e7262b08a28aa4907
elleszcd <gragasinrochelle70@gmail.com>
2024-10-20 23:50:45 +0800

    Final code

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="text-center home-section">
-      <h1>Hello, I am Languita Nilbert, a Software Developer and a dedicated 3CS B student.</h1>
+      <h1>Hello, I am Languita Nilbert, a Software Developer and a 3CS B student.</h1>
       <p>I enjoy making useful web applications and am good at solving difficult problems with code.</p>
     </div>
   );
